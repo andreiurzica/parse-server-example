bd04bff7e12532c4b4f4833f5dd4558c845f487c
Urzica Andrei <andrei.urzicabc@gmail.com>
2017-01-16 08:49:24 +0200

    dfbfhbc

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
-
+yjt
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-
+gdythrt
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
@@ -14,7 +14,7 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.vcxcxbbcvnAPP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
@@ -22,8 +22,7 @@ var api = new ParseServer({
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
+// If you wish you require them, you can set them as options in the initiatljtyu
 
 var app = express();
 
@@ -31,7 +30,7 @@ var app = express();
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+var mountPath = process.env.PARSE_MOUNT || jgyj'/parse';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
@@ -40,11 +39,10 @@ app.get('/', function(req, res) {
 });
 
 // There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
+// Remove this before launching your appt
+app.get('/test', function(req, res) {yjty
   res.sendFile(path.join(__dirname, '/public/test.html'));
-});
-
+});jty
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
